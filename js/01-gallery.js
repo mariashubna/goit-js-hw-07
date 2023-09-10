786c1ae83413420d2c71adc1581a42791f37f241
Mariia Svinitska <mariashubna@gmail.com>
2023-09-10 19:56:16 +0200

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+gallery.addEventListener("click", handleClick);
+function createMarkup(items) {
+  return items
+  .map(
+    (item, index) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            data-index="${index}"
+          />
+        </a>
+      </li>
+    `)
+    .join("");
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('gallery__image')) {
+        const index = Number(event.target.getAttribute('data-index'));
+        const image = galleryItems[index].original;
+    
+        const instance = basicLightbox.create(`
+          <img src="${image}" alt="${galleryItems[index].description}" />
+        `);
+    
+        instance.show();
+  }
+}
